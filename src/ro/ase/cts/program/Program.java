ecb53389cd097f6546732e43a6be4ba85bccaae3
Maria Radu <radu.mariia@gmail.com>
2021-03-08 22:32:58 +0200

    commit1

---
+++ src/ro/ase/cts/program/Program.java
@@ -0,0 +1,22 @@
+package ro.ase.cts.program;
+
+import java.io.FileNotFoundException;
+import java.util.List;
+import ro.ase.cts.clase.Angajat;
+import ro.ase.cts.clase.readers.AngajatiReader;
+
+public class Program {
+
+	public static void main(String[] args) {
+		List<Angajat> listaAngajati;
+		try {
+			listaAngajati = AngajatiReader.readAngajati("angajati.txt");
+			for(Angajat angajat:listaAngajati)
+				System.out.println(angajat.toString());
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
